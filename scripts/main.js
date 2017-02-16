029b3981a50076a1d5d33da14fe094590d9954fc
Tony Toscano <tonymichael16@gmail.com>
2017-02-15 19:56:01 -0500

    Trying to display the JSON data

--- scripts/main.js
+++ scripts/main.js
@@ -1,51 +1,51 @@
-function geoFindMe() {
-    var output = document.getElementById("out");
+// function geoFindMe() {
+//     var output = document.getElementById("out");
 
-    if (!navigator.geolocation) {
-        output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
-        return;
-    }
+//     if (!navigator.geolocation) {
+//         output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
+//         return;
+//     }
 
-    function success(position) {
-        var latitude = position.coords.latitude;
-        var longitude = position.coords.longitude;
-        var timestamp = position.timestamp;
-        var date = new Date(timestamp).toLocaleString();
+//     function success(position) {
+//         var latitude = position.coords.latitude;
+//         var longitude = position.coords.longitude;
+//         var timestamp = position.timestamp;
+//         var date = new Date(timestamp).toLocaleString();
 
-        output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
+//         output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
 
-        var img = new Image();
-        img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
+//         var img = new Image();
+//         img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
 
 
-        output.appendChild(img);
+//         output.appendChild(img);
 
-        displayWeather(latitude, longitude);
-    }
+//         displayWeather(latitude, longitude);
+//     }
 
-    function error() {
-        output.innerHTML = "Unable to retrieve your location";
-    }
+//     function error() {
+//         output.innerHTML = "Unable to retrieve your location";
+//     }
 
-    output.innerHTML = "<p>Locating…</p>";
+//     output.innerHTML = "<p>Locating…</p>";
 
-    navigator.geolocation.getCurrentPosition(success, error);
+//     navigator.geolocation.getCurrentPosition(success, error);
 
-    function displayWeather(latitude, longitude) {
-        var weather = "http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&APPID=fe8de29f7b69b5e5b6898e1d9af5d072";
-        var json;
-        output = document.getElementById("out2");
-        $.ajax({
-            type: 'POST',
-            url: weather,
-            dataType: 'json',
-            success: function(results) {
-                console.log(results);
-                output.innerHTML = "<p>" + JSON.stringify(results.weather[0].description) + "</p>";
-            }
-        });
-    }
-}
+//     function displayWeather(latitude, longitude) {
+//         var weather = "http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&APPID=fe8de29f7b69b5e5b6898e1d9af5d072";
+//         var json;
+//         output = document.getElementById("out2");
+//         $.ajax({
+//             type: 'POST',
+//             url: weather,
+//             dataType: 'json',
+//             success: function(results) {
+//                 console.log(results);
+//                 output.innerHTML = "<p>" + JSON.stringify(results.weather[0].description) + "</p>";
+//             }
+//         });
+//     }
+// }
 
 function getWeather()
 {
@@ -53,11 +53,12 @@ function getWeather()
   var country  = document.getElementById('countryCode').value;
   var weather = "http://api.openweathermap.org/data/2.5/weather?zip=" + zip + "," + country + "&APPID=fe8de29f7b69b5e5b6898e1d9af5d072";
   $.ajax({
-    type: 'POST',
+    type: 'GET',
     url: weather,
     dataType: 'json',
     success: function(results) {
       console.log(results);
+      document.getElementById("out").innerHTML = "<p>" + results.weather[0].main + "</p>";
     }
   });
 }
